fc5b77c051175f82d48b86dc8f17f6a2908f0f9d
LuanBarroso <luan23.lba@gmail.com>
2024-04-08 18:40:24 -0400

    add: new operator logics

--- lib/c_operadores/c_operadores_logicos.dart
+++ lib/c_operadores/c_operadores_logicos.dart
@@ -1,6 +1,7 @@
 void main() {
 // Operadores lógicos
 // && (E)
+// || (OU) Uma das condições precisa ser verdade, ou as duas podem ser verdade, o contrario disso ser falso.
 
   final sexo = 'M';
   final idade = 18;
@@ -9,5 +10,13 @@ void main() {
     print('Pode tirar Habilitação!!!');
   } else {
     ('Nao pode tirar habilitação!!');
+  }
+
+  if (sexo == 'M' || idade >= 18) {
+    print('Pode entrar na festa!!');
+  } else
+    (sexo != 'M' || idade != 18);
+  {
+    print('nao pode entrar na festa!!');
   }
 }
