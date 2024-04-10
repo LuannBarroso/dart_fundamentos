8a140f2ca83f0fde0b6dc5566ca5ae58a5291f67
LuanBarroso <luan23.lba@gmail.com>
2024-04-10 12:35:14 -0400

    feat: editing operators

--- lib/c_operadores/d_ternarios.dart
+++ lib/c_operadores/d_ternarios.dart
@@ -1,6 +1,13 @@
 void main() {
-  final idade = 18;
+  final idade = 12;
 
   final eMaiorDeIdade = (idade >= 18 ? true : false);
-  print('e maior de idade?' + eMaiorDeIdade.toString());
+
+  print('e maior de idade? ' + eMaiorDeIdade.toString());
+
+  print((idade < 13)
+      ? 'criança'
+      : (idade > 12 && idade < 18)
+          ? 'Adolescente'
+          : 'Adulto');
 }
