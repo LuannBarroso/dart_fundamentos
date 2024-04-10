cc37aab05988a71621101c46e56f93edf48f23f3
LuanBarroso <luan23.lba@gmail.com>
2024-04-10 14:49:53 -0400

    aditing informations aware operator

--- lib/d_tratamentos_nulos/b_null_aware_operator.dart
+++ lib/d_tratamentos_nulos/b_null_aware_operator.dart
@@ -1,1 +1,22 @@
-void main() {}
+String? nome;
+void main() {
+  var nomeCompleto = ((nome != null) ? nome! + 'Luan' : 'Luan Barroso');
+  print(nomeCompleto);
+
+  String nomeCompleto2;
+
+  if (nome != null) {
+    nomeCompleto2 = nome! + 'Luan';
+  } else {
+    nomeCompleto2 = 'Luan Barroso';
+  }
+  print(nomeCompleto2);
+
+  var nomeLocal = nome;
+  if (nomeLocal == null) {
+    nomeLocal = 'Luan';
+  }
+  var nomeCompleto3 = nomeLocal + ' Barroso';
+
+  print(nomeCompleto3);
+}
