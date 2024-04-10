4aa7ca1cbec7b2b3cc7ea4f92f72932d83f53b3e
LuanBarroso <luan23.lba@gmail.com>
2024-04-10 14:01:49 -0400

    editing switchs

--- lib/c_operadores/e_switchs.dart
+++ lib/c_operadores/e_switchs.dart
@@ -1,3 +1,16 @@
 void main() {
-  final diaDaSemana = 0;
+  final diaDaSemana = 3;
+  var UhomemMacaco = '';
+
+  switch (diaDaSemana) {
+    case 1:
+    case 2:
+      UhomemMacaco = 'Domingo';
+      print('Dia triste!!');
+      break;
+    default:
+      UhomemMacaco = 'Segunda';
+      print('correu atrás de mim');
+      break;
+  }
 }
