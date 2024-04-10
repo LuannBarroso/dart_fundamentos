01f759c460376bf69a7017e50ce6dc1404112cbf
LuanBarroso <luan23.lba@gmail.com>
2024-04-10 14:02:20 -0400

    feat: create null safety

---
+++ lib/d_tratamento_nulos/a_null_safety.dart
@@ -0,0 +1,10 @@
+void main() {
+  String nome = '';
+//variavel nao nula
+  nome.isEmpty;
+// ja em uma váriavel não nula, podemos acessar metodos dentro dessa variavel não nula
+
+  String? nomeNullo;
+//variavel aceita ser nula
+// porém não podemos acessar metodos de dentro de uma função nula
+}
