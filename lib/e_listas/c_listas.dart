41ad951904bdfaa2db90b0edfd0e160ea5d124c1
LuanBarroso <luan23.lba@gmail.com>
2024-04-11 17:45:04 -0400

    editing listas

--- lib/e_listas/c_listas.dart
+++ lib/e_listas/c_listas.dart
@@ -1,17 +1,14 @@
 void main() {
-  var lista = [1, 2, 3, 4];
-
-  lista.add(5);
-  print(lista);
-
-  final nomes = ['maria', 'joao', 'carlos', 'luan'];
-  nomes.add('luana');
-  print(nomes);
+  final nomes = ['joao', 'maria', 'pedrinho'];
+  nomes.insert(2, 'juninho');
   print(nomes[0]);
   print(nomes[1]);
   print(nomes[2]);
   print(nomes[3]);
-  print('adicionando frabricio na lista');
-  nomes.insert(2, 'fabricio');
   print(nomes);
+  print('Escolhenho primeiro nome');
+  print(nomes.last);
+
+  var primeiroNome = nomes.firstWhere((nomes) => nomes == 'maria');
+  print(primeiroNome);
 }
