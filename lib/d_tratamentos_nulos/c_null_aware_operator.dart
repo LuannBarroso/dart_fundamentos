6d26cea281ad276965fbc053b884c30783f845e2
LuanBarroso <luan23.lba@gmail.com>
2024-04-11 19:43:29 -0400

    editing erros

--- lib/d_tratamentos_nulos/c_null_aware_operator.dart
+++ lib/d_tratamentos_nulos/c_null_aware_operator.dart
@@ -3,10 +3,12 @@ String? nome;
 void main() {
   var sobrenome = 'Barroso';
   //var nomeCompleto = ((nome != null) ? nome! + 'Luan' : 'Luan Barroso');
-  var nomeCompleto = (nome ?? 'Luan') + sobrenome;
+  var nomeCompleto = (nome ?? 'Luan ') + sobrenome;
   //se ele for nulo, será retornado 'LUAN', se NÃO for nulo, será rertonado o SOBRENOME.
   // forma base de utilizar o AWARE OPERATOR.
 
-  String? nomeCompleto2 = null;
+  String? nomeCompleto2;
   print(nomeCompleto2 ?? 'Luan Barroso');
+
+  print(nomeCompleto);
 }
