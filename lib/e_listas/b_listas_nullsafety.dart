c208c2ad03358bc9bf4d993977b6d1f03c51e68f
LuanBarroso <luan23.lba@gmail.com>
2024-04-11 19:50:44 -0400

    modified itens

--- lib/e_listas/b_listas_nullsafety.dart
+++ lib/e_listas/b_listas_nullsafety.dart
@@ -3,17 +3,23 @@ void main() {
 
   // Não aceita nulo.
   List<String> nomes = [];
+  print(nomes);
 
-  List<String>? nomesNulos = null;
+  List<String>? nomesNulos;
   // ? = nullable (Aceita nulo)
+  print(nomesNulos);
 
   //Lista não pode ser nula, nem os itens.
   List<String> nomesInternosNaoAceitaNulos = ['LUAN'];
   var nomesInternosNaoAceitaNulos1 = <String>['LUAN'];
-
+  print(nomesInternosNaoAceitaNulos1);
+  print(nomesInternosNaoAceitaNulos);
   //<String?> Lista não pode ser nula mas os itens internos podem ser nulos.
   var nomesInternosAceitaNulos = <String?>['Luan', 'Maycon', 'null', 'Rodrigo'];
+  print(nomesInternosAceitaNulos);
+
   //<String?>? Tuda na lista pode ser nula e os itens internos podem ser nulos.
-  List<String?>? nomesInternosAceitaNulos1 = null;
+  List<String?>? nomesInternosAceitaNulos1;
+  print(nomesInternosAceitaNulos1);
   // Preciso sempre declarar na frente o tipo de lista quando ela toda for nula.
 }
