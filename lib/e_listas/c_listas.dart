692327fc91742533c994421e744f39b1a5b309ee
LuanBarroso <luan23.lba@gmail.com>
2024-04-11 18:28:04 -0400

    add list coments

--- lib/e_listas/c_listas.dart
+++ lib/e_listas/c_listas.dart
@@ -1,6 +1,8 @@
 void main() {
+  //Criando uma lista de nomes comum
   final nomes = ['joao', 'maria', 'pedrinho'];
   nomes.insert(2, 'juninho');
+  //escolhendo como retornará na tela a lista de nomes
   print(nomes[0]);
   print(nomes[1]);
   print(nomes[2]);
@@ -9,6 +11,18 @@ void main() {
   print('Escolhenho primeiro nome');
   // print(nomes.last);
 
+  // aqui utilizando o fistWhere chamamos o primeiro item da lista, que no caso seria o nome ou numero.
   var primeiroNome = nomes.firstWhere((nomes) => nomes == 'maria');
   print(primeiroNome);
+
+  final numerosGerados = List.generate(10, (index) => index + 1);
+  print(numerosGerados);
+  // Aqui geramos uma lista de nomes ou numeros em ordem crescente, da forma que pedimos a contagem.
+  final stringsGerados = List.generate(20, (index) => 'luan');
+  print(stringsGerados);
+
+  //Aqui podemos utilizar o ...(três pontos seguidos)para juntar uma lista com a outra na mesma linha de código.
+  var listaNumerosSpreadB = [4, 5, 6];
+  var listaNumerosSpread = [1, 2, 3, ...listaNumerosSpreadB];
+  print(listaNumerosSpread);
 }
