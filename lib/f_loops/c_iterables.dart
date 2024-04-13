0dd87972a567bae391e2b7ee2c4426f5ad2dcbc7
LuanBarroso <luan23.lba@gmail.com>
2024-04-12 20:46:35 -0400

    edit iterables

--- lib/f_loops/c_iterables.dart
+++ lib/f_loops/c_iterables.dart
@@ -2,5 +2,6 @@ void main() {
   var numeros = List.generate(10, (index) => index);
 //filtro do numero 5, utilizando o where
 //depois realizado um looping e printou oq precisava ser printado.
-  numeros.where((numero) => numero != 5).forEach((numero) => print(numero));
+  //numeros.where((numero) => numero != 5).forEach((numero) => print(numero));
+  print(numeros);
 }
