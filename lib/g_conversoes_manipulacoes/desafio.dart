b757dc5f1360df4b894d2e3f844b7aad31d50bf0
LuanBarroso <luan23.lba@gmail.com>
2024-04-15 14:37:15 -0400

    editing dart challenge

--- lib/g_conversoes_manipulacoes/desafio.dart
+++ lib/g_conversoes_manipulacoes/desafio.dart
@@ -1,4 +1,6 @@
 void main() {
+  //! Detalhe sobre a String
+  //! A String é composta por 4 campos (Nome|Idade|Profissão|Estado onde mora)
   final pacientes = [
     'Rodrigo Rahman|35|desenvolvedor|SP',
     'Manoel Silva|12|estudante|MG',
@@ -9,14 +11,15 @@ void main() {
     'Regina Verne|35|dentista|MG',
     'João Rahman|55|jornalista|SP',
   ];
-// Aacientes com mais de 20 anos
-  print('Pacientes com mais de 20 anos:');
-  pacientes.forEach((paciente) {
-    var campos = paciente.split('|');
-    var nome = campos[0];
-    var idade = int.parse(campos[1]);
+  //! Baseado no array acima monte um relatório onde:
+  //! 1 - Apresente os pacientes com mais de 20 anos e print o nome deles
+  print('1 - Apresente os pacientes com mais de 20 anos e print o nome deles');
+  for (final paciente in pacientes) {
+    final pacienteDados = paciente.split('|');
+    final idade = int.tryParse(pacienteDados[1]) ?? 0;
     if (idade > 20) {
-      print(nome);
+      print(pacienteDados[0]);
     }
-  });
+  }
+  print(' ');
 }
